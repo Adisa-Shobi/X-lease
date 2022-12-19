0798a1ade2ee83e62263f3ddf133f69024ca8a24
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-12-19 15:37:18 +0100

    Added the index and the state api

---
+++ api/v1/views/index.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Index Route"""
+
+from api.v1.views import app_views
+from flask import jsonify, make_response
+from models import storage
+from models.category import Category
+from models.city import City
+from models.country import Country
+from models.item import Item
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ API Status """
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def api_obj_stats():
+    """ Return the statistics of all available objects """
+    objs = {'User': User,
+           'Categories': Category,
+           'Countries': Country,
+           'Cities': City,
+           'States': State,
+           'Items': Item,
+           'Reviews': Review
+    }
+    obj_count = {k: storage.count(v) for k, v in objs.items()}
+    return make_response(jsonify(obj_count), 200)
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -27,14 +27,16 @@ class DBStorage:
 
     def __init__(self):
         """Instantiate a DBStorage object"""
-        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
-        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
-        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
-        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
+        MYSQL_USER = getenv('MYSQL_USER')
+        MYSQL_PWD = getenv('MYSQL_PWD')
+        MYSQL_HOST = getenv('MYSQL_HOST')
+        MYSQL_DB = getenv('MYSQL_DB')
+        
+        
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                      .format(HBNB_MYSQL_USER, HBNB_MYSQL_PWD,
-                                      HBNB_MYSQL_HOST,
-                                      HBNB_MYSQL_DB)
+                                      .format(MYSQL_USER, MYSQL_PWD,
+                                      MYSQL_HOST,
+                                      MYSQL_DB)
                                       )
 
     def all(self, cls=None):
